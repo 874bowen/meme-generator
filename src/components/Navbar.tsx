010f01a5a04d8117a9dc73d5bec4f5d2efa250a5
Ivan Bowen <bowenivan16@gmail.com>
2023-01-20 10:02:19 +0300

    add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import logo from "../assets/troll-face.png";
 
 const Navbar = () => {
-   return (
-      <nav className="nav">
-         
-      </nav>
-   );
-}
+	return (
+		<nav className="nav">
+			<img src={logo} alt="" width="50px" />
+			<h3>Meme Generator</h3>
+		</nav>
+	);
+};
 
 export default Navbar;
