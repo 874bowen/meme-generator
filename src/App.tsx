e051929ad17195a9215edb20b03e4d0c8c6558ff
Ivan Bowen <bowenivan16@gmail.com>
2023-01-27 23:00:40 +0300

    update memegen application

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 import { Boxes } from "./boxes";
 import Memes from "./components/Memes";
 import Navbar from "./components/Navbar";
-import Squares from "./components/Squares";
+
 
 function App () {
    const [squares, setSquares] = useState(Boxes);
    
    return (
       <>
-      <Navbar />
-      <Memes />
-      <Squares squares={squares} darkMode={true} />
+         <Navbar />
+         <Memes />
       </>
    );
 }
--- src/components/Memes.tsx
+++ src/components/Memes.tsx
@@ -9,13 +9,6 @@ const Memes = () => {
       randomImage: ""
    });
 
-   const [formData, setFormData] = useState({
-      email: "",
-      password: "",
-      confirm_password: "",
-      isSubscribed: false
-   })
-
    const allMemeImages = memesData.data.memes;
 
    function getRandomImage() {
@@ -26,52 +19,28 @@ const Memes = () => {
          randomImage: `${allMemeImages[randomIndex].url}`
       })));
    }
-
-   function validate(password: string, confirm_password: string) {
-      return password === confirm_password;
-   }
-
-   function handleOnSubmit (e: any) {
-      e.preventDefault();
-      let isValidated: boolean = validate(formData.password, formData.confirm_password)
-      if (isValidated){
-         alert(JSON.stringify(formData))
-      } else {
-         alert("Passwords did not match");
-         return
-      }
-
-      if (formData.isSubscribed){
-         alert("Thank you for subscribing to our newsletter");
-      }
-   }
-
-   function handleOnChange (e: any) {
-      const {type, name, value, checked} = e.target;
-      setFormData((prevFormData) => ({
-         ...prevFormData,
-         [name]: type === "checkbox" ? checked : value
+ 
+   function handleOnChange(e: any) {
+      let {name, value} = e.target;
+      setMeme((prevMeme) => ({
+         ...prevMeme,
+         [name]: value
       }))
+      
    }
 
    return(
       <div className="form">
          <div className="form__input-div">
-            <input type="text" name="top-text" id="top-text" placeholder="Shut up" />
-            <input type="text" name="bottom-text" id="bottom-text" placeholder="and take my money" />
+            <input type="text" name="topText" id="topText" placeholder="Shut up" value={meme.topText} onChange={handleOnChange} />
+            <input type="text" name="bottomText" id="bottomText" placeholder="and take my money" value={meme.bottomText} onChange={handleOnChange} />
          </div>
          <button onClick={getRandomImage}><h4>Get a new meme image</h4></button>
-         <img src={meme.randomImage} alt=""  />
-         <form onSubmit={handleOnSubmit}>
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" value={formData.email} onChange={handleOnChange} />
-            <label htmlFor="password">Password: </label>
-            <input type="password" name="password" id="password" value={formData.password} onChange={handleOnChange} />
-            <label htmlFor="confirm_password">Confirm Password: </label>
-            <input type="password" name="confirm_password" id="confirm_password" value={formData.confirm_password} onChange={handleOnChange}  />
-            <input type="checkbox" name="isSubscribed" id="isSubscribed" checked={formData.isSubscribed} onChange={handleOnChange} />
-            <button>Sign Up</button>
-         </form>
+         <div className="form__image-div">
+            <img src={meme.randomImage} className="form__meme-image" alt="" />
+            <div className="form__top-text form__meme--text">{meme.topText}</div>
+         <div className="form__bottom-text form__meme--text">{meme.bottomText}</div>
+         </div>
       </div>
    );
 }
